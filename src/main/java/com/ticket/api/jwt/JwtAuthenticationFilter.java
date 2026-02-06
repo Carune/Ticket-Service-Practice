3bd2312353b8e770a24b5222ab74016202f6631c
KJH <carune99@gmail.com>
2026-02-06 22:19:07 +0900

    Monitoring infro & test

--- src/main/java/com/ticket/api/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ticket/api/jwt/JwtAuthenticationFilter.java
@@ -5,23 +5,32 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 헤더에서 토큰 추출
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/actuator"); // /actuator로 시작하면 필터 생략
+    }
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        // 헤더에서 토큰 추출
+        String token = jwtTokenProvider.resolveToken(request);
+
+        // 유효한 토큰인지 확인
         if (token != null && jwtTokenProvider.validateToken(token)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
